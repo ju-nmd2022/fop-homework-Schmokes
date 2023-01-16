993d764ecdeea26c8f5f8669e74b92851b6f5a0c
Schmokes <lesa22kz@student.ju.se>
2023-01-16 16:56:51 +0100

    Emoji exercise

---
+++ 2-emoji.js
@@ -0,0 +1,24 @@
+background(255, 255, 255);
+
+//face
+fill(240, 216, 11);
+strokeWeight(3);
+ellipse(380, 350, 500);
+
+//left eye
+fill(255, 255, 255);
+ellipse(300, 300, 120);
+
+fill(0, 0, 0);
+ellipse(300, 265, 50);
+
+//rigth eye
+fill(255, 255, 255);
+ellipse(450, 300, 120);
+
+fill(0, 0, 0);
+ellipse(450, 265, 50);
+
+//mouth
+strokeWeight(5);
+line(300, 450, 450, 450);
